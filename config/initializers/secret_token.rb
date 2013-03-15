8d8fd7bfa1cf9f9b89d20d4cce28126f20050bef
TOMITA Masahiro <tommy@tmtm.org>
2013-03-16 08:18:30 +0900

    rails new nseg-golf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NsegGolf::Application.config.secret_token = '2885e2484a690bb936e7aeb60e70d196ec9e55a4a4b96d11b9323a216532e53dfa2d49a7a079d6397579fb35c32cf6d32e80f5e3707df772743b28a7d297b087'
