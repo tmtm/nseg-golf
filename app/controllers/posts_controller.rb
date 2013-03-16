aa9927163b93358616dd9c4f0144d5974d5f295c
TOMITA Masahiro <tommy@tmtm.org>
2013-03-16 09:28:19 +0900

    disable json

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,28 @@
 class PostsController < ApplicationController
   # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all.sort_by(&:size)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @posts }
     end
   end
 
   # GET /posts/1
-  # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @post }
     end
   end
 
   # GET /posts/new
-  # GET /posts/new.json
   def new
     @post = Post.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @post }
     end
   end
 
@@ -38,7 +32,6 @@ class PostsController < ApplicationController
   end
 
   # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new
     user, lang, code = params[:post].values_at(:user, :language, :code)
@@ -49,16 +42,13 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render json: @post, status: :created, location: @post }
       else
         format.html { render action: "new" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PUT /posts/1
-  # PUT /posts/1.json
   def update
     @post = Post.find(params[:id])
     user, lang, code = params[:post].values_at(:user, :language, :code)
@@ -71,23 +61,19 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
 
     respond_to do |format|
       format.html { redirect_to posts_url }
-      format.json { head :no_content }
     end
   end
 end
